721496d47735b054b242ade77cfea3ebc8a9f061
pawelkami <pawelkami2@gmail.com>
2015-03-23 20:11:45 +0100

    Dodano do skryptu uruchamiajacego server opcje sprawdzenia jego skladni

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-lighttpd -D -f lighttpd.conf
+
+if [ "$#" -gt 1 ]
+then
+	echo "Usage: ./run-server.sh option"
+	exit 1 
+fi
+
+if [ "$1" = "check" ]
+then
+	lighttpd -t -f lighttpd.conf
+else
+	lighttpd -D -f lighttpd.conf
+fi
+
+exit 0
