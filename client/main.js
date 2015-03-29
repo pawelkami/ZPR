24831b5bace6a7773ef9f9cf88eba459320bdbc9
carlos <carlos@carlos-Lenovo-G580>
2015-03-29 07:08:59 +0200

    Dodałem okienko do wybrania nicku

--- client/main.js
+++ client/main.js
@@ -1,12 +1,27 @@
 var turn = "";
+var player1 = { nickname: "" };
+var player2 = { nickname: "" };
+var gameStarted = false;
 
 function new_game(){
   $(".col").map(function() {
-    $(this).text("")
+    $(this).text("");
   });
 }
 
+$("#start-button").click(function() {
+  if($("#nickname-textbox").val().length < 1)
+    alert("Podaj swoje imie");
+  else {
+    player1.nickname = ($("#nickname-textbox")).val();
+    $('#nickname-form').delay(150).fadeOut(400);
+    gameStarted = true;
+  }
+});
+
 $(".col").click(function(){
+  if(!gameStarted)
+    return;
   var row = $(this).parent().index();
   var col = $(this).index();
 
