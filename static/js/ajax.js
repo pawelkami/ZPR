2c05cb070cd3116117395cb980303b50c69ac06d
Carlos Zaldivar <czaldiva@mion.elka.pw.edu.pl>
2015-06-03 19:42:34 +0200

    Posprzatano troche html i js

---
+++ static/js/ajax.js
@@ -0,0 +1,93 @@
+new_game = function() {
+  $(".col").map(function() {
+    $(this).text("");
+  });
+  $.ajax({
+    type: "POST",
+    data: {id: player1.id},
+    url: urlReset,
+    async: false
+  });
+};
+
+getOpponentsName = function() {
+  $.ajax({
+    type: "POST",
+    url: urlGetName,
+    data: {id: player1.id},
+    success: function( out_data ) {
+      out_data = $.parseJSON(out_data);
+
+      if(out_data.x === -1 || out_data.y === -1){ // jesli zaczynamy nowa gre to przy pobraniu poczatkowych wartosci jeszcze raz pobierz
+        setTimeout(getOpponentsName, 2000);
+      }
+
+      if( out_data.playerName === "" ){
+        setTimeout(getOpponentsName, 2000);
+      } else {
+        player2.nickname = out_data.playerName;
+        stopWaitingAnimation();
+        gameStarted = true;
+        if(player1.sign !== turn)
+        $(".player1Turn").fadeTo(200, 0);
+        else
+        $(".player2Turn").fadeTo(200, 0);
+      }
+    },
+    error: function(){
+      setTimeout(getOpponentsName, 2000);
+    }
+  });
+};
+
+getOpponentsMove = function() {
+  $.ajax({
+    type: "POST",
+    data: {id: player1.id},
+    url: urlGetMove,
+    success: function( out_data ) {
+      out_data = $.parseJSON(out_data);
+      if(out_data.sign === player2.sign) {
+        $("#game").children().each(function() {
+
+          if($(this).index() === out_data.y)
+          $(this).children().each(function() {
+            if($(this).index() === out_data.x)
+            $(this).text(player2.sign);
+          });
+        });
+
+        setStatus( out_data, player2 );   //ustawia okienko wygranej jesli taka nastapi
+
+        if( out_data.status === "STILL_PLAYING" ){
+          turn = player1.sign;
+          $(".player1Turn").fadeTo(200, 1);
+          $(".player2Turn").fadeTo(200, 0);
+        }
+      }
+      else setTimeout(getOpponentsMove, 2000);
+    },
+    error: function() {
+      setTimeout(getOpponentsMove, 2000);
+    }
+  });
+}
+
+postMove = function(row, col){
+  var output = "";
+  $.ajax({
+    type: "POST",
+    async: false,
+    data: {x: col, y: row, id: player1.id},
+    url: urlMove,
+  }).done(function( out_data ){
+    out_data = $.parseJSON(out_data);
+    output = out_data.status;
+
+    if(out_data.status === "False"){
+      return;
+    }
+    setStatus( out_data, player1);
+  });
+  return output;
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,18 @@
 var turn = "O";
-var player1 = { nickname: "", sign: ""};
-var player2 = { nickname: "", sign: ""};
+var player1 = { nickname: "", sign: "", id: -1 };
+var player2 = { nickname: "", sign: "" };
 var gameStarted = false;
 var status = "";
 
-$(".player1Turn").text(player1.sign);  // pierwszemu graczowi przypisujemy kolko
-$(".player2Turn").text(player2.sign);  // drugiemu graczowi przypisujemy krzyzyk
+$('#game-result-form').hide();
+
+for(var i = 0; i < 16; ++i) {
+  $( "#game" ).append("<div class = 'row'></div>");
+}
+for(var i = 0; i < 16; ++i)
+{
+  $(".row").append("<div class = 'col'></div>");
+}
 
 
 //wciśnięcie entera po wprowadzeniu nazwy gracza, powinno dac sam efekt co klikniecie "start"
@@ -15,6 +22,85 @@ $("#nickname-textbox").keyup(function (e) {
   if (e.keyCode == 13) {
     $("#start-button").trigger("click");
   }
+});
+
+$("#start-button").click(function() {
+  if($("#nickname-textbox").val().length < 1)
+  alert("Podaj swoje imie");
+  else {
+    player1.nickname = ($("#nickname-textbox")).val();
+    $(".player1Text").text(player1.nickname);
+
+    // zapisuje imię gracza na serwerze i zwraca mu jego id oraz znak
+    $.ajax({
+      type: "POST",
+      async: false,
+      data: {name: player1.nickname},
+      url: urlRegister,
+      success: function( out_data ) {
+        out_data = $.parseJSON(out_data);
+        player1.id = out_data.id;
+        player1.sign = out_data.sign;
+        if(out_data.sign === "O") {
+          player2.sign = "X";
+        } else {
+          player2.sign = "O";
+        }
+      }
+    });
+
+    startWaitingAnimation();
+    getOpponentsName();
+    $('#nickname-form').delay(150).fadeOut(400);
+
+    $('.player1Points').text("0");
+    $('.player2Points').text("0");
+    $(".player1Turn").text(player1.sign);
+    $(".player2Turn").text(player2.sign);
+
+
+    if(turn === player2.sign){
+      getOpponentsMove();
+    }
+  }
+});
+
+$(".col").click(function(){
+  if(!gameStarted || turn !== player1.sign){
+    return;
+  }
+  var row = $(this).parent().index();
+  var col = $(this).index();
+
+  if($(this).text().length === 1){
+    alert("To miejsce jest juz zajete, prosze wykonac ruch gdzie indziej.");
+    return;
+  }
+
+  var ret = postMove(row,col);
+
+  if(ret !== "False"){
+    $(this).text(player1.sign);
+    turn = player2.sign;
+    if( ret === "STILL_PLAYING"){
+      getOpponentsMove();
+    }
+  }
+});
+
+$("#new-game-button").click(function() {
+  $('#game-result-form').hide();
+  new_game();
+  turn = "O";
+  if(player1.sign === "X"){
+    $(".player1Turn").fadeTo(200, 0);
+    $(".player2Turn").fadeTo(200, 1);
+    getOpponentsMove();
+  } else {
+    $(".player1Turn").fadeTo(200, 1);
+    $(".player2Turn").fadeTo(200, 0);
+  }
+
 });
 
 var timer;
@@ -30,4 +116,22 @@ startWaitingAnimation = function() {
 stopWaitingAnimation = function() {
   clearInterval(timer);
   $(".player2Text").stop().fadeTo(0, 1).text(player2.nickname);
+}
+
+setStatus = function(out_data, player) {  // funkcja ustawia okienko statusu gry, wlacza sie gdy jest wygrana lub remis
+  if( out_data.status === "VICTORY" ){
+    $('#result-text').text(player.nickname + " WIN");
+    $('#game-result-form').fadeTo(200, 0.9);
+    if(player.sign === player1.sign){
+      $('.player1Points').text(out_data.points);
+    } else {
+      $('.player2Points').text(out_data.points);
+    }
+  } else if( status === "DRAW" ) {
+    $('#result-text').text("DRAW");
+    $('#game-result-form').fadeTo(200, 0.9);
+  } else {
+    $(".player1Turn").fadeTo(200, 0);
+    $(".player2Turn").fadeTo(200, 1);
+  }
 }
